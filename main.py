7c5a375fe5f23a52a5181f0ff1478a050e935371
fernandocar86 <fernandocarranza86@gmail.com>
2022-11-02 19:37:41 -0300

    goles exactos

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pandas as pd
 from os import listdir
 
+global dict_resultados
+global dict_estimaciones
+
 def iniciar():
     definir_puntajes()
     cargar_resultados()
-    cargar_estimaciones()
 
 def definir_puntajes():
     print('Si querés usar los puntajes estándar, ingresá s; si querés ingresar tus propios puntajes ingresa n')
     respuesta1 = input()
     if respuesta1 == 's':
+        global exacto 
         exacto = 10 # coincidencia goles ambos equipos
+        global gpe
         gpe = 5 # coincidencia ganador, perdedor o empate
+        global goles
         goles = 2 # coincidencia goles de un equipo
         armar_puntajes(exacto, gpe, goles)
     elif respuesta1 == 'n':
@@ -43,15 +48,16 @@ def cargar_resultados():
         ruta_entera = ruta + file
         data = pd.read_csv(ruta_entera)
         df = pd.DataFrame(data)
-        cargar_partidos(df, mensaje_resultados)
+        dict_resultados = []
+        cargar_partidos(df, mensaje_resultados, dict_resultados)
+    cargar_estimaciones(dict_resultados)
 
 def cargar_datos(path):
     filenames = listdir(path)
     return [ filename for filename in filenames if filename.endswith( ".csv" ) ]
 
-def cargar_partidos(df, mensaje):
+def cargar_partidos(df, mensaje, dict_partidos):
     lista_partidos = []
-    dict_partidos = []
     for index, row in df.iterrows():
         if not pd.isnull(row[3]): 
             part = row[0]+': '+row[1]+ " vs "+row[2]
@@ -61,9 +67,19 @@ def cargar_partidos(df, mensaje):
     print('los partidos para los que hay '+mensaje+' son los siguientes:')
     for item in lista_partidos:
         print(item)
-    print(dict_partidos)        
+    print(dict_partidos)
+#    return dict_partidos    
+
+def comparar_result_estim(dict_resultados, dict_estimaciones, file):
+    puntaje = 0
+    for item1 in dict_resultados:
+        for item2 in dict_estimaciones:
+                if item1[0] == item2[0] and item1[1] == item2[1] and item1[2] == item2[2]:
+                    puntaje = puntaje + exacto
+    print(puntaje)
+                     
         
-def cargar_estimaciones():
+def cargar_estimaciones(dict_resultados):
     print('Indicá en qué carpeta están alojadas las estimaciones de los participantes')
     ruta2 = input()
     estimaciones = cargar_datos(ruta2)
@@ -72,7 +88,11 @@ def cargar_estimaciones():
         ruta_entera2 = ruta2 + file
         data2 = pd.read_csv(ruta_entera2)
         df2 = pd.DataFrame(data2)
-        cargar_partidos(df2, mensaje_estimaciones)
+        dict_estimaciones = []
+        cargar_partidos(df2, mensaje_estimaciones, dict_estimaciones)
+        comparar_result_estim(dict_resultados, dict_estimaciones, file)
+
+
 
 iniciar()
 #definir_puntajes()
