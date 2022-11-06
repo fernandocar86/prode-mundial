7066f91f99908498a70321f5da3a6b1a4b5ecc7a
fernandocar86 <fernandocarranza86@gmail.com>
2022-11-06 10:47:45 -0300

    más de un ganador

--- visualizacion.py
+++ visualizacion.py
@@ -15,7 +15,24 @@ def armado_html(archivo):
     home.write('</table>\n')
 #    print(df2)
     puntaje_ganador = df2.iloc[0,1]
-    home.write('<p>Ganó el prode nada más ni nada menos que '+str(df2.iloc[0,0])+' con '+str(puntaje_ganador)+' puntos</p>\n')
+    ganador = [str(df2.iloc[0,0])]
+    for index, row in df2.iterrows():
+        if index != 0 and row[1] == df2.iloc[0,1]:
+            ganador.append(str(df2.iloc[index,0]))
+    if len(ganador) == 1:
+        home.write('<p>Ganó el prode nada más ni nada menos que '+str(df2.iloc[0,0])+' con '+str(puntaje_ganador)+' puntos</p>\n')
+    else:
+        prefijo = ganador[0:-1]
+        sufijo = ganador[-1]
+        print(sufijo)
+        ganadores = ''
+        for item in prefijo:
+            if item != prefijo[-1]:
+                ganadores = ganadores+str(item)+', '
+            else:
+                ganadores = ganadores+str(item)
+        ganadores = ganadores+' y '+str(sufijo)
+        home.write('<p>Ganaron el prode nada más ni nada menos que '+ganadores+' con '+str(puntaje_ganador)+' puntos</p>\n') 
     home.write('</body> \n </html>')
     home.close()
  
